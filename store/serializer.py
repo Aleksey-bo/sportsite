98c957ea3a9886b28b2cdd44099a395e05a42947
Al.Meshch <gvozdea1243@gmail.com>
2024-05-09 16:12:25 +0300

    update store.serializer, add user

--- store/serializer.py
+++ store/serializer.py
@@ -6,6 +6,7 @@ from .models import (
     City
 )
 from account.serializer import UserSerializer
+from account.models import CustomUser
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -40,11 +41,13 @@ class ProductSerializer(serializers.ModelSerializer):
         category_data = validated_data.pop('category')
         location_data = validated_data.pop('location_product')
         images_data = validated_data.pop('image', None)
+        user_data = validated_data.pop('user')
 
         category = Category.objects.get(category_name=category_data.get('category_name'))
         location = City.objects.get(location=location_data.get('location'))
+        user = CustomUser.objects.get(id=user_data)
 
-        product = Product.objects.create(category=category, location_product=location, **validated_data)
+        product = Product.objects.create(category=category, location_product=location, user=user, **validated_data)
 
         if images_data:
             for image_data in images_data:
